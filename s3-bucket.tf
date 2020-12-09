35d13a0877cd14609d2b7b02bb12b95ca0a2ea5e
Vivienne Owusu-Ansah <41501236+voa2000@users.noreply.github.com>
2020-12-09 22:30:17 +0000

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
   source        = "app.terraform.io/rank-interactive/s3-bucket/aws"
   version       = "1.17.0"
-  bucket_prefix        = prefix 
+  bucket_prefix        = var.prefix 
 }
