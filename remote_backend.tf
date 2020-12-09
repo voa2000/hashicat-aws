4251b0c473d345f23fe931a23ea49d20a86a834d
Vivienne Owusu-Ansah <voa2000@hotmail.com>
2020-12-09 19:16:16 +0000

    adding remote backend

--- remote_backend.tf
+++ remote_backend.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "rank-interactive/www-prod"
+    organization = "rank-interactive"
     workspaces {
       name = "hashicat-aws"
     }
