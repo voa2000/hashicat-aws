d32d499eaf9a6ec12fe6d02363d992f11ba5a0d9
Vivienne Owusu-Ansah <41501236+voa2000@users.noreply.github.com>
2021-06-15 08:19:17 +0100

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "rank-interactive"
+    organization = "rank-interactive-poc"
     workspaces {
       name = "hashicat-aws"
     }
